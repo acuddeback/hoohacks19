133ac5e7b0ad777386a9b205cde7365e17671a6b
acuddeback <atc@cuddeback.org>
2019-03-02 13:58:05 -0500

    fixed buggy page urls

--- hoohacks/equitunity/urls.py
+++ hoohacks/equitunity/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'login/$', views.login, name='login'),
     url(r'signup/$', views.signup, name='signup'),
-    url(r'user_profile/$', views.userProfile, name='user_profile'),
-    url(r'school_profile/$', views.schoolProfile, name='school_profile')
+    url(r'user_profile/$', views.user_profile, name='user_profile'),
+    url(r'school_profile/$', views.school_profile, name='school_profile')
 ]
