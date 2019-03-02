35369cb47a9452b63ef828fc2aef059f587dfe09
acuddeback <atc@cuddeback.org>
2019-03-02 13:01:09 -0500

    added major model and user fiels for collge students

--- hoohacks/equitunity/models.py
+++ hoohacks/equitunity/models.py
@@ -10,23 +10,33 @@ class Ratio(models.Model):
 
 class School(models.Model):
   name = models.TextField(max_length=50, blank=True)
-  actCode = models.PositiveIntegerField(blank=True)
+  actCode = models.PositiveIntegerField(blank=True, unique=True)
   state = models.TextField(max_length=2, blank=True)
   address = models.TextField(max_length=50, blank=True)
   website = models.TextField(max_length=50, blank=True)
   acceptanceRate = models.FloatField(max_length=4, blank=True)
-  gRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name ="gRatio") # gender ratio
-  fsRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name ="fsRatio") # factulty-student ratio
+  gRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name="gRatio") # gender ratio
+  fsRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name="fsRatio") # factulty-student ratio
   classSize = models.PositiveIntegerField(blank=True)
+
+class Major(models.Model):
+  name = models.TextField(max_length=50, blank=True, unique=True)
 
 class User(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   address = models.TextField(max_length=50, blank=True)
   inCollege = models.BooleanField(default=False)
+  # college student fields, 
   college = models.ForeignKey(School, on_delete=models.CASCADE)
+  major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
+  secondMaj = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="secondMaj")
+  minor = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="minor")
+  secondMin = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="secondMin")
+  hometown = models.TextField(max_length=2, blank=True)
+  interests = models.TextField(max_length=2, blank=True)
 
   def __str__(self):
-    if(self.isAdvisor):
+    if(self.inCollege):
       return self.user.first_name + " is a college student"
     return self.user.first_name + " is a high school student"
 
