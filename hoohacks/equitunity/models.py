0ecb2ac632d70b23dd27d6d7b95e477095137d19
acuddeback <atc@cuddeback.org>
2019-03-02 18:08:15 -0500

    a lot of stuff

--- hoohacks/equitunity/models.py
+++ hoohacks/equitunity/models.py
@@ -5,46 +5,47 @@ from django.contrib.auth.models import User
 
 
 class Ratio(models.Model):
-    numer = models.PositiveIntegerField(blank=True)
-    denom = models.PositiveIntegerField(blank=True)
+    numer = models.PositiveIntegerField(null=True)
+    denom = models.PositiveIntegerField(null=True)
 
 
 class School(models.Model):
-    name = models.TextField(max_length=50, blank=True)
-    actCode = models.PositiveIntegerField(blank=True, unique=True)
-    state = models.TextField(max_length=2, blank=True)
-    address = models.TextField(max_length=50, blank=True)
-    website = models.TextField(max_length=50, blank=True)
-    acceptanceRate = models.FloatField(max_length=4, blank=True)
+    name = models.TextField(max_length=50, null=True)
+    actCode = models.PositiveIntegerField(null=True, unique=True)
+    state = models.TextField(max_length=2, null=True)
+    address = models.TextField(max_length=50, null=True)
+    website = models.TextField(max_length=50, null=True)
+    acceptanceRate = models.FloatField(max_length=4, null=True)
     gRatio = models.ForeignKey(
         Ratio, on_delete=models.CASCADE, related_name="gRatio")  # gender ratio
     fsRatio = models.ForeignKey(
         Ratio, on_delete=models.CASCADE, related_name="fsRatio")  # factulty-student ratio
-    classSize = models.PositiveIntegerField(blank=True)
-    imgUrl = models.TextField(max_length=1000, blank=True)
+    classSize = models.PositiveIntegerField(null=True)
+    imgUrl = models.TextField(max_length=1000, null=True)
 
 
 class Major(models.Model):
-    name = models.TextField(max_length=50, blank=True, unique=True)
+    name = models.TextField(max_length=50, null=True, unique=True)
 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=True, auto_now=True)
     inCollege = models.BooleanField(default=False)
     # college student fields,
     college = models.ForeignKey(School, on_delete=models.CASCADE)
     major = models.ForeignKey(
-        Major, on_delete=models.CASCADE, related_name="major", blank=True)
+        Major, on_delete=models.CASCADE, related_name="major", null=True)
     secondMaj = models.ForeignKey(
-        Major, on_delete=models.CASCADE, related_name="secondMaj", blank=True)
+        Major, on_delete=models.CASCADE, related_name="secondMaj", null=True)
     minor = models.ForeignKey(
-        Major, on_delete=models.CASCADE, related_name="minor", blank=True)
+        Major, on_delete=models.CASCADE, related_name="minor", null=True)
     secondMin = models.ForeignKey(
-        Major, on_delete=models.CASCADE, related_name="secondMin", blank=True)
-    hometown = models.TextField(max_length=50, blank=True)
-    interests = models.TextField(max_length=140, blank=True)
-    bio = models.TextField(max_length=400, blank=True)
-    gradYear = models.PositiveIntegerField(blank=True)
+        Major, on_delete=models.CASCADE, related_name="secondMin", null=True)
+    hometown = models.TextField(max_length=50, null=True)
+    interests = models.TextField(max_length=140, null=True)
+    bio = models.TextField(max_length=400, null=True)
+    gradYear = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         if(self.inCollege):
--- hoohacks/equitunity/views.py
+++ hoohacks/equitunity/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
 import json 
+from .models import *
 import requests
+
 # Create your views here.
 
 
@@ -36,8 +40,28 @@ def signup_submit(request):
   if request.method == 'POST':
     print ('WE GOT STUFF: ', request.POST)
     data = request.POST
-    # get each item individually
+    passw = request.POST.get('inputPassword')
+    confp = request.POST.get('confirmPassword')
     print('data is', data)
+    print("\n PASSWORDS", passw, confp)
+    if(passw!=confp):
+      print("Passwords don't match")
+      return render(request, 'signup.block.html', {'error':True})
+    
+    username = request.POST.get('inputEmail')
+    first_name = request.POST.get('firstName')
+    last_name = request.POST.get('lastName')
+    password = passw
+    email = request.POST.get('inputEmail')
+    user = User.objects.create(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+    dob = request.POST.get('dob')
+    if(request.POST.get('accountType')=="college"):
+      inCollege=True
+    else:
+      inCollege=False
+    Student.objects.create(user=user, dob=dob, inCollege=inCollege)
+    # get each item individually
+    return render(request, 'user_profile.block.html')
   else:
     print('You should make sure you have the right form method')
   return render(request, 'user_profile.block.html')
