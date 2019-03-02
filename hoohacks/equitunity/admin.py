28375fb5e7d1416391e2e8ef0798b872c230a32b
Chad Baily <chadbaily27@gmail.com>
2019-03-02 15:41:50 -0500

    Add admin

--- hoohacks/equitunity/admin.py
+++ hoohacks/equitunity/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(School)
+admin.site.register(Ratio)
+admin.site.register(Major)
