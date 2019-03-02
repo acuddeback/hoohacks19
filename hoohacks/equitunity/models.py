8772625b845845f17f72f86075e3490a03f6ece3
Will Scheib <wms9gv@virginia.edu>
2019-03-02 15:17:14 -0500

    merge

--- hoohacks/equitunity/models.py
+++ hoohacks/equitunity/models.py
@@ -3,41 +3,50 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Ratio(models.Model):
-  numer = models.PositiveIntegerField(blank=True)
-  denom = models.PositiveIntegerField(blank=True)
+    numer = models.PositiveIntegerField(blank=True)
+    denom = models.PositiveIntegerField(blank=True)
+
 
 class School(models.Model):
-  name = models.TextField(max_length=50, blank=True)
-  actCode = models.PositiveIntegerField(blank=True, unique=True)
-  state = models.TextField(max_length=2, blank=True)
-  address = models.TextField(max_length=50, blank=True)
-  website = models.TextField(max_length=50, blank=True)
-  acceptanceRate = models.FloatField(max_length=4, blank=True)
-  gRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name="gRatio") # gender ratio
-  fsRatio = models.ForeignKey(Ratio, on_delete=models.CASCADE, related_name="fsRatio") # factulty-student ratio
-  classSize = models.PositiveIntegerField(blank=True)
-  imgUrl = models.TextField(max_length=300, blank=True)
+    name = models.TextField(max_length=50, blank=True)
+    actCode = models.PositiveIntegerField(blank=True, unique=True)
+    state = models.TextField(max_length=2, blank=True)
+    address = models.TextField(max_length=50, blank=True)
+    website = models.TextField(max_length=50, blank=True)
+    acceptanceRate = models.FloatField(max_length=4, blank=True)
+    gRatio = models.ForeignKey(
+        Ratio, on_delete=models.CASCADE, related_name="gRatio")  # gender ratio
+    fsRatio = models.ForeignKey(
+        Ratio, on_delete=models.CASCADE, related_name="fsRatio")  # factulty-student ratio
+    classSize = models.PositiveIntegerField(blank=True)
+    imgUrl = models.TextField(max_length=1000, blank=True)
+
 
 class Major(models.Model):
-  name = models.TextField(max_length=50, blank=True, unique=True)
+    name = models.TextField(max_length=50, blank=True, unique=True)
+
 
 class Student(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  inCollege = models.BooleanField(default=False)
-  # college student fields, 
-  college = models.ForeignKey(School, on_delete=models.CASCADE)
-  major = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="major")
-  secondMaj = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="secondMaj")
-  minor = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="minor")
-  secondMin = models.ForeignKey(Major, on_delete=models.CASCADE, related_name="secondMin")
-  hometown = models.TextField(max_length=50, blank=True)
-  interests = models.TextField(max_length=140, blank=True)
-  bio = models.TextField(max_length=400, blank=True)
-  gradYear = models.PositiveIntegerField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    inCollege = models.BooleanField(default=False)
+    # college student fields,
+    college = models.ForeignKey(School, on_delete=models.CASCADE)
+    major = models.ForeignKey(
+        Major, on_delete=models.CASCADE, related_name="major")
+    secondMaj = models.ForeignKey(
+        Major, on_delete=models.CASCADE, related_name="secondMaj")
+    minor = models.ForeignKey(
+        Major, on_delete=models.CASCADE, related_name="minor")
+    secondMin = models.ForeignKey(
+        Major, on_delete=models.CASCADE, related_name="secondMin")
+    hometown = models.TextField(max_length=50, blank=True)
+    interests = models.TextField(max_length=140, blank=True)
+    bio = models.TextField(max_length=400, blank=True)
+    gradYear = models.PositiveIntegerField(blank=True)
 
-  def __str__(self):
-    if(self.inCollege):
-      return self.user.first_name + " is a college student"
-    return self.user.first_name + " is a high school student"
-
+    def __str__(self):
+        if(self.inCollege):
+            return self.user.first_name + " is a college student"
+        return self.user.first_name + " is a high school student"
---
+++ hoohacks/equitunity/serializers.py
@@ -0,0 +1,110 @@
+# Import DRF
+from rest_framework import serializers
+
+# Import models
+from django.contrib.auth.models import User
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'password'
+        )
+
+# Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, required=True)
+
+    class Meta:
+        model = Student
+        fields = (
+            'pk',
+            'user',
+            'inCollege',
+            'college',
+            'major',
+            'secondMaj',
+            'minor',
+            'secondMin',
+            'hometown',
+            'interests',
+            'bio',
+            'gradYear'
+        )
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(
+            UserSerializer(), validated_data=user_data)
+        student = Student.objects.update_or_create(user=user,
+                                                   inCollege=validated_data.pop(
+                                                       'inCollege'),
+                                                   college=validated_data.pop(
+                                                       'college'),
+                                                   major=validated_data.pop(
+                                                       'major'),
+                                                   secondMaj=validated_data.pop(
+                                                       'secondMaj'),
+                                                   minor=validated_data.pop(
+                                                       'minor'),
+                                                   secondMin=validated_data.pop(
+                                                       'secondMin'),
+                                                   hometown=validated_data.pop(
+                                                       'hometown'),
+                                                   interests=validated_data.pop(
+                                                       'interests'),
+                                                   bio=validated_data.pop(
+                                                       'bio'),
+                                                   gradYear=validated_data.pop(
+                                                       'gradYear')),
+        return student
+
+# School
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = (
+            'pk',
+            'name',
+            'actCode',
+            'state',
+            'address',
+            'website',
+            'acceptanceRate',
+            'gRatio',
+            'fsRatio',
+            'classSize',
+            'imgUrl'
+        )
+
+# Ratio
+
+
+class RatioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ratio
+        fields = (
+            'pk',
+            'numer',
+            'denom',
+        )
+
+# Ratio
+
+
+class MajorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Major
+        fields = (
+            'pk',
+            'name',
+        )
