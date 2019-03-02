00a6c07fcae799c7883b6989c7379a68bd54f643
Will Scheib <wms9gv@virginia.edu>
2019-03-02 13:53:36 -0500

    Added html outlines

--- hoohacks/equitunity/urls.py
+++ hoohacks/equitunity/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home')
+    url(r'^$', views.login, name='login')
+    url(r'^$', views.signup, name='signup')
+    url(r'^$', views.userProfile, name='userProfile')
+    url(r'^$', views.schoolProfile, name='schoolProfile')
 ]
--- hoohacks/equitunity/views.py
+++ hoohacks/equitunity/views.py
@@ -6,3 +6,19 @@ from django.shortcuts import render
 def home(request):
       # Return the home page
     return render(request, 'home.block.html')
+
+def login(request):
+      # Return the login page
+    return render(request, 'login.block.html')
+
+def signup(request):
+      # Return the signup page
+    return render(request, 'signup.block.html')
+
+def user_profile(request):
+      # Return the user profile page
+    return render(request, 'userProfile.block.html')
+
+def school_profile(request):
+      # Return the school profile page
+    return render(request, 'schoolProfile.block.html')
